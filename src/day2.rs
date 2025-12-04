0df58c25c587ca685168cfbfe0e91704f21a00c3
Arison <arison646@gmail.com>
2025-12-04 19:43:17 +0100

    day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -37,3 +37,89 @@ pub fn part1() {
     }
     print!("\ninvalid_ids: {:?}", invalid_id)
 }
+
+pub fn part2() {
+    let mut invalid_id = 0;
+    let mut binding = get_file().expect("failed to load file");
+    let mut content = binding.as_mut_str().trim();
+    let mut size: u64;
+    let base: u64 = 10;
+    let mut position: Vec<u64> = Vec::with_capacity(10);
+    let mut factors: Vec<u64>;
+
+    while let Some(i) = content.find(',') {
+        // get both numberes
+        let start: u64 = content[..content.find('-').expect("failed to find -")]
+            .parse()
+            .expect("failed to parse to u64");
+        //print!("{:?}", start);
+        let end: u64 = content[content.find('-').expect("failed to find -") + 1..i]
+            .parse()
+            .expect("failed to parse to u64");
+        //print!("-{:?}\n", end);
+        // iterate over the range
+        for i in start..=end {
+            //print!("number: {:?}\n", i);
+            // get the amount of digits in the number
+            size = i.checked_ilog10().unwrap_or(0) as u64 + 1;
+
+            // get all the factors of the size
+            // if the main focus here would be omtimalisation i would save the outputs
+            // into a dictionary so we don't need to call the function every time
+            // however since size doesn't exceed like 10 i think it's fine to leave like this
+            factors = list_factors(size);
+            /*
+            print!(
+                "
+                \nfactors: {:?}\n",
+                factors
+            );
+            */
+            for factor in factors {
+                if factor == size {
+                    continue;
+                }
+                //print!("factor: {:?}\n",factor);
+                position.clear();
+                let mut temp = i;
+                for j in (factor..=size).step_by((factor) as usize) {
+                    // remove the slice of number from the number and add it to the vec
+                    let slice_of_number = temp % base.pow(j as u32) / base.pow((j - factor) as u32);
+                    temp -= slice_of_number;
+                    position.push(slice_of_number);
+                    //get the part of the number
+                }
+                // check if all the values are the same
+                if position.windows(2).all(|w| w[0] == w[1]) {
+                    invalid_id += i;
+                    break;
+                }
+                /*
+                print!(
+                    "
+                    \nposition: {:?}\n",
+                    position
+                );
+                */
+            }
+        }
+        // get the next slice
+        content = &content[i + 1..];
+    }
+    print!("\ninvalid_ids: {:?}", invalid_id)
+}
+
+fn list_factors(number: u64) -> Vec<u64> {
+    let mut factors: Vec<u64> = Vec::new();
+    let mut i: u64 = 1;
+    while i * i <= number {
+        if number % i == 0 {
+            factors.push(i);
+            if i * i != number {
+                factors.push(number / i);
+            }
+        }
+        i += 1;
+    }
+    factors
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use day1::crack_passord;
-use day2::part1;
+use day2::part2;
 
 mod day1;
 mod day2;
 
 fn main() {
-    part1();
+    part2();
 }
